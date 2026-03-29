cc07d60eaf6f17f9aa74b2eaa644116cf6e8c32a
AbuYahya2 <ww.goldletter@gmail.com>
2026-03-29 06:30:41 +0300

    fix: app working perfectly

--- src/App.js
+++ src/App.js
@@ -1,1 +1,9 @@
-donecat
+import React,{useState} from 'react';
+import Auth from './pages/AuthPage';
+import Editor from './pages/EditorPage';
+import Community from './pages/CommunityPage';
+export default function App(){
+const [user,setUser]=useState(null);
+if(!user) return <Auth onLogin={setUser}/>;
+return <Editor user={user} onLogout={()=>setUser(null)}/>;
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-donecat
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './styles/global.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter basename="/lum3t-harir">
+    <App />
+  </BrowserRouter>
+);
